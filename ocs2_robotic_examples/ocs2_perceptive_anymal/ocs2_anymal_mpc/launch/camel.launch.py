1bb244c4907c2c5ea121c32c672647b4b818ab91
lyciswx-byte <lyciswx@gmail.com>
2026-01-17 09:38:56 +0800

    fix: update launch configuration and dependencies for anymal_c model

--- ocs2_robotic_examples/ocs2_perceptive_anymal/ocs2_anymal_mpc/launch/camel.launch.py
+++ ocs2_robotic_examples/ocs2_perceptive_anymal/ocs2_anymal_mpc/launch/camel.launch.py
@@ -10,8 +10,8 @@ def load_file_content(file_path):
         return file.read()  
 
 def generate_launch_description():
-    target_command = get_package_share_directory('ocs2_anymal_mpc') + "/config/c_series/targetCommand.info"
-    description_name = get_package_share_directory('ocs2_anymal_models') + "/urdf/anymal_camel_rsl.urdf"
+    target_command = get_package_share_directory('ocs2_anymal_mpc') + "/config/anymal_c/targetCommand.info"
+    description_name = get_package_share_directory('ocs2_robotic_assets') + "/resources/anymal_c/urdf/anymal_c.urdf"
     ld = launch.LaunchDescription([
         launch.actions.IncludeLaunchDescription(
             launch.launch_description_sources.PythonLaunchDescriptionSource(
@@ -20,8 +20,9 @@ def generate_launch_description():
             ),
             launch_arguments={
                 'robot_name': 'camel',
-                'config_name': 'c_series',
+                'config_name': 'anymal_c',
                 'description_name': description_name,
+                'urdf_model_path': description_name,
                 'target_command': target_command
             }.items()
         )
